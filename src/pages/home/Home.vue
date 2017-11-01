e8ea49085f32ad2562925da3efe77708179cf5a9
cindy-zf <1127460674@qq.com>
2017-11-01 21:51:00 +0800

    change

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+	  <home-header></home-header>
+	  <swiper-content></swiper-content>
+  </div>
+</template>
+
+<script>
+	import HeaderComponent from "./Header";
+	import SwiperComponent from "./Swiper";
+export default {
+   	components:{
+   		"home-header":HeaderComponent ,
+   		"swiper-content":SwiperComponent 
+   	}
+}
+</script>
+
+<style>
+		
+</style>
